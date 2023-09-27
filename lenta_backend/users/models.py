389bd44b4de38eae367e7af11d4f3bc9f3969516
nsts <ananastasiia13@gmail.com>
2023-09-28 00:24:35 +0400

    Add User model

--- lenta_backend/users/models.py
+++ lenta_backend/users/models.py
@@ -1,3 +1,40 @@
-from django.db import models
+from django.contrib.auth import models
 
-# Create your models here.
+
+class User(models.AbstractUser):
+    first_name = models.CharField(
+        'Имя',
+        max_length=150,
+        blank=False,
+        null=False,
+    )
+    last_name = models.CharField(
+        'Фамилия',
+        max_length=150,
+        blank=False,
+        null=False,
+    )
+    email = models.EmailField(
+        'Адрес электронной почты',
+        blank=False,
+        null=False,
+    )
+    username = models.CharField(
+        'Логин',
+        blank=False,
+        null=False,
+    )
+    position = models.CharField(
+        'Должность',
+        max_length=150,
+        blank=False,
+        null=False,
+    )
+
+    class Meta:
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
+        ordering = ('pk',)
+
+    def __str__(self):
+        return f'{self.first_name, self.last_name}, {self.position}'
