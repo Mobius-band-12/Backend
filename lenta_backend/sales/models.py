3014d56079bf0bec6fb5c3c6cd3c3b8df0d3b04f
nsts <ananastasiia13@gmail.com>
2023-09-28 21:50:27 +0400

    Add Sale model

--- lenta_backend/sales/models.py
+++ lenta_backend/sales/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from stores.models import Store
+from products.models import Product
 
-# Create your models here.
+
+class Sale(models.Model):
+    store_id = models.ForeignKey(
+        Store,
+        to_field='store_id',
+        on_delete=models.CASCADE,
+        verbose_name='id магазина',
+        max_length=32,
+    )
+    product_id = models.ForeignKey(
+        Product,
+        to_field='product_id',
+        on_delete=models.CASCADE,
+        verbose_name='id товара',
+        max_length=32,
+    )
+    date = models.DateField(
+        'Дата продажи',
+    )
+    is_promo = models.BooleanField(
+        'Флаг промо',
+    )
+    sales_in_units = models.PositiveSmallIntegerField(
+        'Число проданных товаров без признака промо',
+    )
+    promo_sales_in_units = models.PositiveSmallIntegerField(
+        'Число проданных товаров с признаком промо',
+    )
+    sales_in_rub = models.PositiveIntegerField(
+        'Продажи без признака промо в руб.',
+    )
+    promo_sales_in_rub = models.PositiveIntegerField(
+        'Продажи с признаком промо в руб.',
+    )
+
+    class Meta:
+        verbose_name = 'продажи'
+        verbose_name_plural = 'продажи'
+        ordering = ('pk',)
+
+    def __str__(self):
+        return f'Продажи товара {self.product_id} за {self.date}'
+
+
+class Forecast(models.Model):
+    pass
