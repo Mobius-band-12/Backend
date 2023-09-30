b5913f88993b045be8571164e1d846fa845f79a6
nsts <ananastasiia13@gmail.com>
2023-09-30 20:03:23 +0400

    Add viewsets and serializers for User, Sale, Store

--- lenta_backend/api/views.py
+++ lenta_backend/api/views.py
@@ -1,7 +1,20 @@
 from rest_framework import mixins
+from users.models import User
 from stores.models import Store
+from sales.models import Sale
+import serializers
+
+
+class UserViewSet(mixins.RetrieveModelMixin):
+    queryset = User.objects.all()
+    serializer_class = serializers.UserSerializer
 
 
 class StoreViewSet(mixins.ListModelMixin):
     queryset = Store.objects.all()
     serializer_class = serializers.StoreSerializer
+
+
+class SaleViewSet(mixins.ListModelMixin):
+    queryset = Sale.objects.all()
+    serializer_class = serializers.SaleSerializer
